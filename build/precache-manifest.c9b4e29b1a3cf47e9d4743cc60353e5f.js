111e1d24ff325d1f0094df199666137ed8b801a7
shopcuber-cms <zhanglongji@juanet.com>
2020-04-14 14:18:00 +0800

    打包

---
+++ build/precache-manifest.c9b4e29b1a3cf47e9d4743cc60353e5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8758be008c92a1ca7a34051c5d4eb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ef6731bddc5e0f8afcd",
+    "url": "/static/css/main.8d1c7909.chunk.css"
+  },
+  {
+    "revision": "1b62d5c56410c7498739",
+    "url": "/static/js/2.422b15d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.422b15d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef6731bddc5e0f8afcd",
+    "url": "/static/js/main.f6b8a85e.chunk.js"
+  },
+  {
+    "revision": "fa0af807e30880911a9d",
+    "url": "/static/js/runtime-main.28faca3d.js"
+  }
+]);
